dfc695bc20cff32949f510b072b759fdcc19fa80
Maple IT Firm <mapleitfirm.pc12@gmail.com>
2023-07-28 11:36:07 +0600

    module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { BooksController } from './modules/books/books.controller';
+import { BooksService } from './modules/books/books.service';
+import { BooksModule } from './modules/books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, BooksModule],
+  controllers: [AppController, BooksController],
+  providers: [AppService, BooksService],
 })
 export class AppModule {}
